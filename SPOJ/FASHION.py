b2be5ec9a5334e93cf002588758f4ecfd52261ec
Sarthak Khanna <khannasarthak.1994@gmail.com>
2017-01-09 22:09:23 -0700

    1 SPOJ
    Day 2

---
+++ SPOJ/FASHION.py
@@ -0,0 +1,17 @@
+import time
+start_time = time.time()
+###############CODE STARRS##############
+op = []
+for i in range(t):
+    n = int(input())
+    m =list(map(int,input().split(' ')))
+    w =list(map(int,input().split(' ')))
+    m.sort()
+    w.sort()
+    hot = []
+    op.append(sum(a*b for a,b in zip(m,w)))
+for i in op:
+    print (i,sep='\n')
+
+###############CODE ENDS################
+print("--- %s seconds ---" % (time.time() - start_time))t = int(input())
