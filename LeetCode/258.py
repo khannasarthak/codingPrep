483f86c08a0ed6d02044a81e05c1cb36dc2e68f8
Sarthak Khanna <khannasarthak.1994@gmail.com>
2017-01-12 19:23:23 -0700

    Revert "Day 3 : Leetcode"
    This reverts commit d92a97d7274ed367d43243e18b64edf60dd46ce8.

--- LeetCode/258.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def addDigits(self, num):
-        while (len(str(num))!=1):
-        	s = 0
-        	for i in str(num):
-        		s += int(i)
-        	num = s
-        return (num)
